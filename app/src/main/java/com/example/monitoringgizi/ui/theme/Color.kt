894f59369111fba6575893659256e4d5329887f9
Reno Nilam Sari <renonilamsari2806@gmail.com>
2025-01-04 13:48:45 +0700

    Initial commit: Catatan Gizi App by Reno Nilam Sari

---
+++ app/src/main/java/com/example/monitoringgizi/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.monitoringgizi.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFE3A1BE)
+val PurpleGrey80 = Color(0xFF71B6D1)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFFE3A1BE)
+val PurpleGrey40 = Color(0xFF71B6D1)
+val Pink40 = Color(0xFF7D5260)
+
+val PinkE3A1BE = Color(0xFFE3A1BE)
+val Blue71B6D1 = Color(0xFF71B6D1)
